00c2ba920ea73cae91a5f325e380ebe41025ce35
Velkatta <Velkatta@users.noreply.github.com>
2019-12-17 00:08:43 +0200

    Add files via upload

---
+++ EvenNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12._Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+            while (number % 2 == 1)
+            {
+                Console.WriteLine("Please write an even number.");
+                number = Math.Abs(int.Parse(Console.ReadLine()));
+            }
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number is: {number}");
+            }
+        }
+    }
+}
---
+++ ForeignLanguages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06._Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+            switch (country)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+                case "Mexico":
+                case "Spain":
+                case "Argentina":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
